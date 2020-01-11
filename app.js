f57403df851758a9e97cdb1767afdbe81436ad9a
Micah Bala <micahkulien@gmail.com>
2020-01-11 19:11:34 +0100

    Implement CORS

--- app.js
+++ app.js
@@ -20,6 +20,20 @@ app.use((req, res, next) => {
   next(error);
 });
 
+// implement cross origin resource sharing CORS
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization'
+  );
+  res.setHeader(
+    'Access-Control-Allow-Methods',
+    'GET, POST, PUT, DELETE, PATCH, OPTIONS'
+  );
+  next();
+});
+
 app.use((error, req, res, next) => {
   res.status(error.status || 500);
   res.send({
